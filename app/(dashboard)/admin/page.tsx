a5319ff4cf5d46361904dec2e8930a9e99c0e1f7
fuatcangnc <fuatcankod@gmail.com>
2024-08-11 17:44:34 +0300

    category admin panel

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,10 +1,8 @@
-"use client"
 import React from 'react'
-import { MediaLibrary } from '@/components/admin/media-library'
 function Admin() {
   return (
     <div>
-            <MediaLibrary onSelect={(file) => console.log('Selected file:', file)} />
+            Admin Panel
 
     </div>
   )
