8ebe81a3cfc7f9b8e9800015f13b30f8318e4b5f
fuatcangnc <fuatcankod@gmail.com>
2024-08-12 13:52:54 +0300

    added sitemap and robots.txt

---
+++ app/(sitemap)/sitemap.xml/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server'
+
+export const revalidate = 36;
+
+export async function GET() {
+  const baseUrl = process.env.SITE_URL || 'https://fuatgenc.vercel.app'
+  const currentDate = new Date().toISOString()
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <sitemap>
+        <loc>${baseUrl}/post-sitemap.xml</loc>
+        <lastmod>${currentDate}</lastmod>
+      </sitemap>
+      <sitemap>
+        <loc>${baseUrl}/page-sitemap.xml</loc>
+        <lastmod>${currentDate}</lastmod>
+      </sitemap>
+      <sitemap>
+        <loc>${baseUrl}/category-sitemap.xml</loc>
+        <lastmod>${currentDate}</lastmod>
+      </sitemap>
+    </sitemapindex>`
+
+  return new NextResponse(sitemap, {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/xml',
+    },
+  })
+}
