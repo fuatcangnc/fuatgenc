2c203da108089efb5b9d6c38cfe81f02240b1156
fuatcangnc <fuatcankod@gmail.com>
2024-08-12 23:22:34 +0300

    resend

--- utils/nodemailer.ts
+++ utils/nodemailer.ts
@@ -1,6 +1,8 @@
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+
+const resendApiKey = process.env.RESEND_API_KEY;
+const resend = new Resend(resendApiKey);
 
 export async function sendEmail({ to, subject, text, html }: {
   to: string;
